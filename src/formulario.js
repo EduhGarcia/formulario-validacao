eea8b6780f4b957bdbcfc0ae9b54ca76545555a3
Eduardo Garcia <eg836151@gmail.com>
2023-05-05 14:07:43 -0300

    formulario com validaçao

---
+++ src/formulario.js
@@ -0,0 +1,25 @@
+const caixaMensagens = document.querySelectorAll(".caixa-msg")
+const botaoEnviar = document.querySelector(".botao-enviar")
+const campoObrigatorio = document.querySelectorAll(".campo-obrigatorio")
+
+caixaMensagens.forEach((item, indice) => {
+    item.addEventListener('change', () =>{
+        if(item.value !== ''){
+            caixaMensagens[indice].classList.remove('vazia')
+            campoObrigatorio[indice].classList.remove('ativado')
+            item.classList.add('caixa-escrita')
+        }
+    })
+    
+})
+
+botaoEnviar.addEventListener("click", function(enviar){
+    enviar.preventDefault();
+    caixaMensagens.forEach(function(item, indice){
+        
+        if(item.value === ''){
+            campoObrigatorio[indice].classList.add('ativado')
+            caixaMensagens[indice].classList.add('vazia')
+        }
+    })
+})
